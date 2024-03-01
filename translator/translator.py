4c82811b40aed4b0d40498de2beaf491b3684d9a
blondyboy <kalabokmaksim@gmail.com>
2024-03-01 22:02:07 +0300

    update readme

--- translator/translator.py
+++ translator/translator.py
@@ -2,8 +2,8 @@ from __future__ import annotations
 
 import sys
 
-from translator.ast_parser import AstParser
-from translator.lexer import TokenEnum, Tokenizer
+from ast_parser import AstParser
+from lexer import TokenEnum, Tokenizer
 
 sys.path.append("../csa_lab3")
 
@@ -17,7 +17,7 @@ from machine.isa import (
     OffsetInstructionWithAdMon,
     SecondWord,
 )
-from translator.nodes import (
+from nodes import (
     AssignNode,
     BinaryOp,
     InitNode,
@@ -349,6 +349,10 @@ def main(source, target, debug_file):
     with open(source, encoding="UTF-8") as fr:
         code = fr.read()
     tokens = Tokenizer(code).start_analyze()
+    j = 0
+    for i in tokens:
+        j += 1
+        print(f"{j} = {i}")
     ast_tree = AstParser(tokens).parse_code()
     instr_list = Translator(ast_tree)
     instr_list.ast_to_list(instr_list.ast)
@@ -357,12 +361,13 @@ def main(source, target, debug_file):
     s_w = SecondWord(45000)
     hlt = Instruction(OPCODE.HLT)
     instr_list = [pre, s_w, *instr_list, hlt]
-    with open(target, "wb") as fw, open(debug_file, "w") as file_debug:
+    with open(target, "br+") as fw, open(debug_file, "w") as file_debug:
         j = 0
         for instruction in instr_list:
             file_debug.write(f" {j} - {instruction.get_bytes_value().hex()} - {instruction}\n")
-            fw.write(instruction.get_bytes_value())
+            print(f" {j}: {instruction}")
             j += 1
+            fw.write(instruction.get_bytes_value())
 
 
 if __name__ == "__main__":
